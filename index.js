16ed440145566a4c1056cbb11632e1d73d12e84f
KSmith8888 <83607802+KSmith8888@users.noreply.github.com>
2023-02-01 17:33:12 -0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,34 @@
+import dotenv from "dotenv";
+dotenv.config();
+import express from "express";
+import rateLimit from "express-rate-limit";
+import cors from "cors";
+
+import { weather } from "./routes/weather-route.js";
+import { nasa } from "./routes/nasa-route.js";
+
+const app = express();
+
+app.use(express.json());
+
+app.use(cors({ origin: process.env.ORIGIN }));
+
+const limiter = rateLimit({
+    windowMs: 60000,
+    max: 5,
+    standardHeaders: true,
+    legacyHeaders: false,
+});
+
+app.use(limiter);
+
+app.get("/", (req, res) => {
+    return res.json({ success: "This is not the data you're looking for" });
+});
+
+app.use("/weather", weather);
+app.use("/nasa", nasa);
+
+app.listen(process.env.PORT, () =>
+    console.log(`App listening on port ${process.env.PORT}`)
+);
