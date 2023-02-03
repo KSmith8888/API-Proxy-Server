4ef9cc2f1bbf001da06b5bcd4607f2e7a10cafe3
KSmith8888 <83607802+KSmith8888@users.noreply.github.com>
2023-02-02 18:04:44 -0800

    Added caching and updated error handling

--- routes/nasa-route.js
+++ routes/nasa-route.js
@@ -1,21 +1,34 @@
 import express from "express";
-const nasa = express.Router();
-import fetch from "node-fetch";
 
-const fetchNasaData = async () => {
-    const url = `https://api.nasa.gov/planetary/apod?api_key=${process.env.NASA_API_KEY}`;
-    try {
-        const nasaStream = await fetch(url);
-        const nasaJson = await nasaStream.json();
-        return nasaJson;
-    } catch (err) {
-        return { Error: err };
-    }
-};
+const nasa = express.Router();
+let nasaCache;
+let cacheTime;
 
 nasa.get("/", async (req, res) => {
-    const data = await fetchNasaData();
-    res.json(data);
+    const currentTime = Date.now();
+    try {
+        if (cacheTime && currentTime - cacheTime < 60000) {
+            throw new Error(
+                "Please wait at least one minute before requesting information again"
+            );
+        }
+    } catch (err) {
+        return res.json(`Error: ${err}`);
+    }
+    const url = `https://api.nasa.gov/planetary/apod?api_key=${process.env.NASA_API_KEY}`;
+    try {
+        const nasaResponse = await fetch(url);
+        if (nasaResponse.ok) {
+            const nasaData = await nasaResponse.json();
+            nasaCache = nasaData;
+            cacheTime = currentTime;
+            return res.json(nasaData);
+        } else {
+            throw new Error(nasaResponse.status);
+        }
+    } catch (err) {
+        return res.json(`Error: ${err}`);
+    }
 });
 
 export { nasa };
