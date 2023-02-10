c3a58d01b4f7b6c10d485261f568178721dc2426
KSmith8888 <83607802+KSmith8888@users.noreply.github.com>
2023-02-09 17:39:05 -0800

    Added movies route

--- index.js
+++ index.js
@@ -3,22 +3,12 @@ dotenv.config();
 import express from "express";
 import rateLimit from "express-rate-limit";
 import cors from "cors";
+
 import { weather } from "./routes/weather-route.js";
 import { nasa } from "./routes/nasa-route.js";
+import { movies } from "./routes/movies-route.js";
 
 const app = express();
-
-app.set("trust proxy", true);
-
-app.use(express.json());
-
-app.use(
-    cors({
-        origin: process.env.ORIGIN,
-        optionsSuccessStatus: 200,
-    })
-);
-
 const limiter = rateLimit({
     windowMs: 60000,
     max: 5,
@@ -26,12 +16,22 @@ const limiter = rateLimit({
     legacyHeaders: false,
 });
 
+app.set("trust proxy", 1);
+
+app.use(express.json());
+app.use(
+    cors({
+        origin: process.env.ORIGIN,
+        optionsSuccessStatus: 200,
+    })
+);
 app.use(limiter);
 
 app.get("/", (req, res) => {
     return res.json({ success: "This is not the data you're looking for" });
 });
 
+app.use("/movies", movies);
 app.use("/weather", weather);
 app.use("/nasa", nasa);
 
